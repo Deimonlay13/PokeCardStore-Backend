1ea11ec4a15174d5c04e480e8cf06857a8abdbf9
Deimonlay13 <di.pizarrom@duocuc.cl>
2025-12-04 00:26:03 -0300

    se creo el repository, dto, controller, service, impl

---
+++ src/main/java/com/gdl/pokecardstore/service/IProductoService.java
@@ -0,0 +1,18 @@
+package com.gdl.pokecardstore.service;
+
+
+import java.util.List;
+
+import com.gdl.pokecardstore.dto.ProductoDTO;
+
+
+public interface IProductoService  {
+
+
+  List<ProductoDTO>getAllProductos();
+
+   
+
+    
+       
+}
