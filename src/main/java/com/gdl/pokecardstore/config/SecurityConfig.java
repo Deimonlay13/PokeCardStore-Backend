2c4762d0933be7e6bdc84e06dd98297684a9958b
GeraldineBecerra <gera.becerra@duocuc.cl>
2026-06-27 09:56:13 -0400

    Se corrige pom

--- src/main/java/com/gdl/pokecardstore/config/SecurityConfig.java
+++ src/main/java/com/gdl/pokecardstore/config/SecurityConfig.java
@@ -30,19 +30,20 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register" "/actuator/health",
-                                "/actuator/prometheus").permitAll()
+                        .requestMatchers(
+                                "/auth/login",
+                                "/auth/register",
+                                "/actuator/health",
+                                "/actuator/prometheus",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**")
+                        .permitAll()
                         .requestMatchers("/Producto/**").permitAll()
                         .requestMatchers("/usuario/**").permitAll()
                         .requestMatchers("/venta/**").permitAll()
                         .requestMatchers("/detalle-venta/**").permitAll()
                         .requestMatchers("/direccion/**").permitAll()
                         .requestMatchers("/pago/**").permitAll()
-                        .requestMatchers(
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**")
-                        .permitAll()
-
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
